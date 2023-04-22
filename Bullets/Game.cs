42e21325d1cbe7207cca1985efbf0c87706d6b24
Peter James <github@flinch.ca>
2023-04-21 19:12:04 -0700

    Initial commit

---
+++ Bullets/Game.cs
@@ -0,0 +1,36 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullets
+{
+    internal class Game
+    {
+        private GameWindow GameWindow { get; }
+
+        public bool IsRunning => GameWindow.IsOpen;
+
+        public Game()
+        {
+            GameWindow = new GameWindow("Bullets", 800, 600);
+        }
+
+        public void Update()
+        {
+            GameWindow.Update();
+        }
+
+        public void Draw()
+        {
+            GameWindow.BeginDraw();
+            
+            // TODO
+            
+            GameWindow.EndDraw();
+        }
+    }
+}
---
+++ Bullets/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Bullets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            
+            while (game.IsRunning)
+            {
+                game.Update();
+                game.Draw();
+            }
+        }
+    }
+}
